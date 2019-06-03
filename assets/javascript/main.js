7e49cc11a09ab3c44a1caa3d04fd3c81df0676de
donxiya <31514669+donxiya@users.noreply.github.com>
2019-06-02 19:14:54 -0500

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -24,14 +24,14 @@ $(document).ready(function () {
         var next = 1;
         var newEntry = { name: name, des: des, firstTT: firstTT, freq: freq, next: next };
         database.ref().push(newEntry);
-        $("#trainName").val(""); 
+        $("#trainName").val("");
         $("#trainDes").val("");
         $("#trainFTT").val("");
         $("#trainFreq").val("");
     }
     var minAway = function (FRQ, NXT, FTT) {
         var arrTime = moment(FTT, "hh:mm").add(FRQ * NXT, "minutes")
-        var now=moment();
+        var now = moment();
         var difference = arrTime.diff(now, "minutes");
 
         if (difference === 0) {
@@ -71,38 +71,47 @@ $(document).ready(function () {
             update.addClass("update");
             update.text("Update");
             //update.attr("data-key", key);
-            $(document).on('click', ".update", function() {
-                console.log("Update btn clicked!");  
+            $(document).on('click', ".update", function () {
+                console.log("Update btn clicked!");
                 console.log("Stored Value:  ", trainName, trainDestination, trainArrival, trainFrequency);
                 $("#inputUpdate").show();
-                $("#trainName").attr("value", trainName); 
+                $("#trainName").attr("value", trainName);
                 $("#trainDes").attr("value", trainDestination);
                 $("#trainFTT").attr("value", trainArrival);
                 $("#trainFreq").attr("value", trainFrequency);
                 $("#inputUpdate").on("click", function () {
-                    console.log("Update btn clicked!"); 
+                    console.log("Update btn clicked!");
                     childData.name = $("#trainName").val();
                     childData.des = $("#trainDes").val();
                     childData.firstTT = $("#trainFTT").val();
                     childData.freq = $("#trainFreq").val();
                     database.ref(key).update(childData);
-                    $("#trainName").val(""); 
+                    $("#trainName").val("");
                     $("#trainDes").val("");
                     $("#trainFTT").val("");
                     $("#trainFreq").val("");
                     $("#inputUpdate").hide();
                     console.log(childData);
-                });    
+                });
             });
-            var remove;
-            if (parseInt(trainAway, 10)<=0){
-                console.log(trainName+" next train! ")
+            // var remove = $("<button>");
+            // remove.addClass("remove");
+            // remove.text("Remove");
+            // //update.attr("data-key", key);
+            // $(document).on('click', ".remove", function () {
+            //     var key = childSnap.key;
+            //     console.log("Stored Value:  ", trainName, trainDestination,trainArrival, trainFrequency);
+            //     database.ref(key).remove();
+            // });
+            if (parseInt(trainAway, 10) <= 0) {
+                console.log(trainName + " next train! ")
                 childData.next++;
                 database.ref(key).update(childData);
 
             }
             $("#scheduleList > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td><td>" + trainArrival + "</td><td>" + trainAway + "</td></tr>");
             $("#scheduleList > tbody").append(update);
+            //$("#scheduleList > tbody").append(remove);
         });
     };
     setInterval(scheduleUpdate, 1000);
